f4f9396cecdfca464bbe476374b70776811ecd43
antchristensen <anthony.christensen1988@gmail.com>
2024-09-18 17:02:16 -0600

    Prep4 finished

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,73 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        
+        List<int> numbers = new List<int>();
+        List<int> positiveNumbers = new List<int>();
+
+        int userNumber = -1;
+
+        while (userNumber != 0)
+        {
+            Console.WriteLine("Enter a list of numbers, type 0 when finished");
+
+            string userResponse = Console.ReadLine();
+            userNumber = int.Parse(userResponse);
+
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+        }
+
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        int max = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+
+        Console.WriteLine($"The largest number is: {max}");
+
+        int smallestPositive = int.MaxValue;
+        bool foundPositive = false;
+
+        foreach (int number in numbers)
+        {
+            if (number > 0)
+            {
+                positiveNumbers.Add(number);
+                foundPositive = true;
+
+                if (number < smallestPositive)
+                {
+                    smallestPositive = number;
+                }
+            }
+        }
+
+        if (foundPositive)
+        {
+            Console.WriteLine($"The smallest number is: {smallestPositive}");
+        }
+
+        
+
+
+
     }
 }
