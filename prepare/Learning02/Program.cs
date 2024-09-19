961df9161bd3e73cd5a3914bad3241736bfb24ae
antchristensen <anthony.christensen1988@gmail.com>
2024-09-18 19:36:12 -0600

    job&prog adding

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Manager";
+        job1._company = "TC&Sons";
+        job1._startYear = 2001;
+        job1._endYear = 2024;
+
+        Job job2 = new Job();
+        job2._company = "MSD";
+        job2._jobTitle = "Bus Driver";
+        job2._startYear =2001;
+        job2._endYear = 2024;
     }
 }
