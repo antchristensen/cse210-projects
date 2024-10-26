db2c03f52ef3adbc34290fd9227df50d7398f334
antchristensen <anthony.christensen1988@gmail.com>
2024-10-25 18:53:29 -0600

    shapes added

--- prepare/Learning06/Program.cs
+++ prepare/Learning06/Program.cs
@@ -1,9 +1,24 @@
 using System;
 
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning06 World!");
+        
+        List<Shape> shapes = new List<Shape>();
+
+        
+        shapes.Add(new Square("Red", 4));
+        shapes.Add(new Rectangle("Blue", 5, 3));
+        shapes.Add(new Circle("Green", 2));
+
+        
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Color: {shape.GetColor()}");
+            Console.WriteLine($"Area: {shape.GetArea()}");
+            Console.WriteLine(); 
+        }
     }
 }
